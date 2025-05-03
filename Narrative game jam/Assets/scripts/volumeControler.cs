c12487ddb5558edd626ae337cefd771bc927d4b8
pradnyesh4 <pradnyeshgaikwad2008@gmail.com>
2025-05-04 01:27:11 +0530

    sound + music slider

--- Narrative game jam/Assets/scripts/volumeControler.cs
+++ Narrative game jam/Assets/scripts/volumeControler.cs
@@ -6,4 +6,24 @@ public class volumeControler : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    void Start()
+    {
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume)*20);
+    }
 }
